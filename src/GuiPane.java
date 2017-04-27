24b0b2164b145f1af113b210322f6a5f2f9aed49
Joel Fiser <joel.fiser@nielsen.com>
2017-04-27 10:40:01 -0400

    Handle the event listener differently

--- src/GuiPane.java
+++ src/GuiPane.java
@@ -1,83 +1,65 @@
 
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-import javax.swing.BorderFactory;
-import javax.swing.JEditorPane;
-import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
-import javax.swing.text.Document;
-import javax.swing.text.html.HTMLEditorKit;
-import javax.swing.text.html.StyleSheet;
+public class GuiPane extends JPanel {
+	
+static String comboSelected = "";
+static String testCaseToMakeValid = "";
 
-public class GuiPane
-{
-static JFrame frame = new JFrame("Browsy4"); 
-static JTextArea textArea = new JTextArea();
-static JTextArea infoTextArea = new JTextArea();
-
-		
- public GuiPane()
-  {
-	  addFrame();
-	  addScrollPane(140,20,25,20, BorderLayout.CENTER);
-	  //addScrollPane(20,20,25,20, BorderLayout.NORTH);
-  }
-  public void addScrollPane(Integer __marginTop, Integer _marginRight, Integer _marginBottom, Integer _marginLeft, String _loc){
-	  SwingUtilities.invokeLater(new Runnable()
-    {
-      public void run()
-      {
-        //JTextArea textArea = new JTextArea();
-    	  GuiPane.textArea.setFont(new Font("SansSerif", Font.PLAIN, 16));
-    	  GuiPane.textArea.setForeground(new Color(0x444444));
-    	  GuiPane.textArea.setSize(400,400);    
-
-    	  GuiPane.textArea.setLineWrap(true);
-    	  GuiPane.textArea.setEditable(true);
-    	  GuiPane.textArea.setVisible(true);
-        // add text to it; we want to make it scroll
-        //textArea.setText("xx\nxx\nxx\nxx\nxx\nxx\nxx\nxx\nxx\nxx\nxx\nxx\nxx\nxx\n");
-        // create a scrollpane, givin it the textarea as a constructor argument
-    	      	      	  
-        JScrollPane scrollPane = new JScrollPane(textArea);
-        scrollPane.setPreferredSize(new Dimension(50, 450));
-        /*scrollPane.setBorder(BorderFactory.createCompoundBorder(
-					            BorderFactory.createCompoundBorder(
-					                            BorderFactory.createTitledBorder("Plain Text"),
-					                            BorderFactory.createEmptyBorder(255,20,20,20)),
-					            scrollPane.getBorder()));*/
-        //scrollPane.setBorder(BorderFactory.createEmptyBorder(350,20,20,20));
-        scrollPane.setBorder(BorderFactory.createEmptyBorder(__marginTop, _marginRight, _marginBottom, _marginLeft));
-
-        
-        
-        //JFrame frame = new JFrame("Browsy");
-        frame.getContentPane().add(scrollPane, _loc);
-        /*frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // set the frame size (you'll usually want to call frame.pack())
-        frame.setSize(new Dimension(840, 680));      
-        // center the frame
-        frame.setLocationRelativeTo(null);  */   
-        frame.setVisible(true);
-      }
+public GuiPane() {
+    //super(new BorderLayout());
+    String[] testCases = { "BC_TestPlayer", "testCase2", "testCase3", "testCase4", "testCase5" };
+    comboSelected = testCases[0];
+    JComboBox testCaseCombo = new JComboBox(testCases);
+    testCaseCombo.setSelectedIndex(0);
+    // Set up the event listener for comboBox
+    testCaseCombo.addActionListener(new ActionListener(){
+        public void actionPerformed(ActionEvent e){
+        	JComboBox cb = (JComboBox)e.getSource();
+            comboSelected = (String)cb.getSelectedItem();
+            //updateLabel(petName);
+            System.out.println("chosenTestCase: " + comboSelected);
+        }
     });
-  }
-  public void addFrame(){
-	  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      // set the frame size (you'll usually want to call frame.pack())
-      frame.setSize(new Dimension(840, 680));      
-      // center the frame
-      frame.setLocationRelativeTo(null);      
-      frame.setVisible(true);
-  }
-  public void addTextToPane(String str){
-	  GuiPane.textArea.append(str);
-  }
+    
+    add(testCaseCombo, BorderLayout.PAGE_START);
+    createAndShowGUI(this);
+}
+private static void createAndShowGUI(GuiPane guiPane) {
+	javax.swing.SwingUtilities.invokeLater(new Runnable() {
+        public void run(){
+		    JFrame frame = new JFrame("All In One Ping Validator");
+		    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		    guiPane.setOpaque(true); //content panes must be opaque
+		    frame.setContentPane(guiPane);
+		    
+		    JButton btnSetBaseline = new JButton("Set as Baseline");
+		    guiPane.add(btnSetBaseline);
+		    btnSetBaseline.setBounds(60, 20, 20, 30);
+		    //btnSetBaseline.addActionListener(new BtnListener(comboSelected));
+		    // Set up the event listener for baseline button
+		    btnSetBaseline.addActionListener(new ActionListener(){
+		        public void actionPerformed(ActionEvent e){
+		        	testCaseToMakeValid = comboSelected;
+		        	System.out.println("testCaseToMakeValid: " + testCaseToMakeValid);
+		        }
+		    });
+		    
+		    //testCaseCombo.setPrototypeDisplayValue("text here");
+		    frame.setSize(600, 600);
+		    //frame.setMinimumSize(new Dimension(600, 600));
+		    //frame.pack();
+		    frame.setVisible(true);
+        }
+	});
+}
 
 }
+
+
+
+
